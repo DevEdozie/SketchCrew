1c1906c910a22a52b8f0c963760564dd00d8c2cf
mac <muhammedowoyale@gmail.com>
2024-08-08 16:38:33 +0100

    Fixed the tab navigation error

--- app/src/main/java/com/example/sketchcrew/ui/screens/HomeActivity.kt
+++ app/src/main/java/com/example/sketchcrew/ui/screens/HomeActivity.kt
@@ -3,10 +3,10 @@ package com.example.sketchcrew.ui.screens
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sketchcrew.R
 import com.example.sketchcrew.databinding.ActivityHomeBinding
-import com.google.android.material.tabs.TabLayout
 
-class HomeActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
+class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
 
@@ -15,31 +15,28 @@ class HomeActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Set up the TabLayout listener
-        binding.bottomTab.addOnTabSelectedListener(this)
-    }
-
-    override fun onTabSelected(tab: TabLayout.Tab?) {
-        when (tab?.position) {
-            0 -> Unit // If there's nothing to do on this tab, you can leave it empty
-            1 -> navigateTo(SketchesActivity::class.java)
-            2 -> navigateTo(ProfileActivity::class.java)
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    true
+                }
+                R.id.explore -> {
+                    navigateTo(SketchesActivity::class.java)
+                    true
+                }
+                R.id.profile -> {
+                    navigateTo(ProfileActivity::class.java)
+                    true
+                }
+                else -> false
+            }
         }
-    }
-
-    override fun onTabUnselected(tab: TabLayout.Tab?) {
-        // Handle tab unselected state if needed
-    }
-
-    override fun onTabReselected(tab: TabLayout.Tab?) {
-        // Handle tab reselected state if needed
     }
 
     // Helper function to navigate to a different activity
     private fun navigateTo(activityClass: Class<*>) {
         val intent = Intent(this, activityClass)
         startActivity(intent)
-        // Optionally finish the current activity
         // finish()
     }
 }
