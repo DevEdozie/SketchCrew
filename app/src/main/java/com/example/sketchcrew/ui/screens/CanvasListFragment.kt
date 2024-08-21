eb29aa3d6051e12ce468f9bd790bf6fdcab120d2
Eric Ibu <ericomartin.0785@gmail.com>
2024-08-21 21:11:38 +0100

    add recyclerview to sketches activity

--- app/src/main/java/com/example/sketchcrew/ui/screens/CanvasListFragment.kt
+++ app/src/main/java/com/example/sketchcrew/ui/screens/CanvasListFragment.kt
@@ -64,70 +64,6 @@ class CanvasListFragment : Fragment() {
             findNavController().navigate(R.id.action_canvasListFragment_to_drawnCanvasFragment)
         }
 
-
-//        lifecycleScope.launch {
-//            viewModel.loadCanvases()
-//            viewModel.canvases.observe(viewLifecycleOwner, Observer { canvases ->
-//                Log.d(TAG, "onCreateView: $canvases")
-//                adapter =
-//                    CanvasListAdapter(canvases, object : CanvasListAdapter.OnItemClickListener {
-//                        override fun onCanvasClick(canvasData: CanvasData) {
-//                            lifecycleScope.launch {
-//                                val pathData = viewModel.getCanvas(canvasData.id)?.paths.toString()
-//                                    .trimIndent()
-////                        val gson = Gson()
-////                        val type = object : TypeToken<List<PathData>>() {}.type
-////                        val pathDataList: List<PathData> = gson.fromJson(pathData, type)
-////                        val pathDataString = convertToPathDataString(pathDataList)
-//                                if (pathData == null) {
-//                                    findNavController().navigate(R.id.action_canvasListFragment_to_drawnCanvasFragment)
-//                                } else {
-//                                    val action =
-//                                        CanvasListFragmentDirections.actionCanvasListFragmentToDrawnCanvasFragment(
-//                                            pathData.toInt()
-//                                        )
-//                                    findNavController().navigate(action)
-//                                }
-//                            }
-//                        }
-//
-//                        override fun onSaveCanvas(canvasData: CanvasData) {
-//                            lifecycleScope.launch {
-//                                viewModel.saveCanvas(canvasData)
-//                            }
-//                        }
-//
-//                        override fun onDeleteCanvas(canvasData: CanvasData) {
-//                            lifecycleScope.launch {
-//                                viewModel.deleteCanvas(canvasData)
-//                            }
-//                        }
-//                    })
-//                recycle.adapter = adapter
-//            })
-//
-//
-////            { canvas ->
-////                // Handle canvas selection (e.g., navigate to drawing screen)
-////
-////            }
-////        })
-//
-//        }
-
-
-//        viewModel.canvases.observe(viewLifecycleOwner) { canvases ->
-//            adapter.submitList(canvases)
-//        }
-//        pathsAdapter = PathsAdapter(pathsList) { pathData ->
-//            val bundle = Bundle().apply {
-//                putString("path_data", pathData.path)
-//                putInt("color", pathData.color)
-//                putFloat("strokeWidth", pathData.strokeWidth)
-//            }
-//            findNavController().navigate(R.id.action_pathsFragment_to_drawingFragment, bundle)
-//        }
-
         return _binding.root
     }
 
@@ -143,112 +79,9 @@ class CanvasListFragment : Fragment() {
         lifecycleScope.launch {
             viewModel.loadDrawings.observe(viewLifecycleOwner){ drawing ->
                 drawAdapter.submitList(drawing)
-//                val pathAdapter = PathAdapter(drawing) { draws ->
-//                    draws?.let { paths ->
-////                        val pathDataList: List<PathData> = Gson().fromJson(
-////                            draws.paths,
-////                            object : TypeToken<List<PathData>>() {}.type
-////                        )
-//                        val pathDataList = PairConverter().fromPaths(draws.paths)
-////                        val dataPath = PairConverter().fromPathList(pathDataList)
-////                        viewModel.getPath()
-//                        pathDataList.forEach { pathData ->
-//
-//                                val pathInfo = pathData.first.toString()
-//                                val colorInfo = pathData.second.color
-//                                val strokeWidth = pathData.second.strokeWidth
-//
-//                                if (pathInfo != null) {
-//                                    // Navigate and pass the arguments
-//
-//                                } else {
-//                                    // Handle the case where path is null
-//                                    // For example, log an error or show a message to the user
-//                                    Log.e("CanvasListFragment", "Path data is null")
-//                                }
-//                        }
-//                    }
-//                }
 
             }
         }
-
-//        viewModel.loadDrawings.observe(viewLifecycleOwner, Observer { drawing ->
-//            drawing?.let {
-//                val pathAdapter = PathAdapter(drawing) { draws ->
-//                    val pathDataList: List<PathData> = Gson().fromJson(draws.paths, object : TypeToken<List<PathData>>() {}.type)
-//                    var pathStr =  ''
-//                    var colooCode = 0
-//                    var widthStroke = 0f
-//                    val pathsAndPaints = pathDataList.map { pathData ->
-//                        val path = stringToPath(pathData.path)
-//                        pathStr = path
-//                        val paint = Paint().apply {
-//                            color = pathData.color
-//                            strokeWidth = pathData.strokeWidth
-//                        }
-//                        Pair(path, paint)
-//                    }
-//
-//                    // Perform your navigation here, passing necessary data
-//                    val action = CanvasListFragmentDirections.actionCanvasListFragmentToDrawnCanvasFragment(
-//                        pathData, pathData.color, pathData.strokeWidth
-//                    )
-//                    findNavController().navigate(action)
-//                }
-//                recycle.adapter = pathAdapter
-//            } ?: run {
-//                // Handle the case where `drawing` is null
-//                println("No drawing found.")
-//            }
-//        })
-
-
-//            viewModel.loadDrawings.observe(requireActivity(), Observer { drawing ->
-//                val pathAdapter = PathAdapter(drawing) { draws ->
-//                    drawing?.let {
-//                        val pathDataList: List<PathData> = Gson().fromJson(draws.paths, object : TypeToken<List<PathData>>() {}.type)
-//                        pathDataList.map { pathData ->
-//                            val path = stringToPath(pathData.path)
-//                            path.toString()
-//                            val paint = Paint().apply {
-//                                color = pathData.color
-//                                strokeWidth = pathData.strokeWidth
-//                            }
-//                            pathData.color
-//                            pathData.strokeWidth
-//                            Pair(path, paint)
-//                        }
-//                    }
-//                    val action =
-//                        CanvasListFragmentDirections.actionCanvasListFragmentToDrawnCanvasFragment(
-//                            "", 0, 0f
-//                        )
-//                    findNavController().navigate(action)
-//                }
-//                recycle.adapter = pathAdapter
-//            }
-//
-//            )
-//            val paths = withContext(Dispatchers.IO) {
-//                db.drawingDao().getAllDrawings()
-//            }
-
-//            pathAdapter = PathAdapter(paths) { pathData ->
-//                // Handle path click
-//                val action =
-//                    CanvasListFragmentDirections.actionCanvasListFragmentToDrawnCanvasFragment(
-//                        pathData.id
-//                    )
-//                findNavController().navigate(action)
-//            }
-//                val intent = Intent(this@PathListActivity, EditPathActivity::class.java).apply {
-//                    putExtra("pathId", pathData.id)
-//                }
-//                startActivity(intent)
-
-
-
     }
 
     private fun stringToPath(pathString: String): Path {
@@ -262,26 +95,5 @@ class CanvasListFragment : Fragment() {
             }
         }
         return path
-    }
-
-//    fun convertToPathDataString(pathDataList: List<PathData>): String {
-//        return buildString {
-//            pathDataList.forEachIndexed { index, pathData ->
-//                // Extract x and y coordinates from mNativePaint
-//                // Assuming mNativePaint encodes coordinates in a specific format, e.g., as integers or floats
-//                // Here, you must replace this example with your actual logic to extract coordinates
-//                val x = pathData.second.mNativePaint.toFloat() // this is a placeholder logic
-//                val y = pathData.second.mNativePaint.toFloat() // this is a placeholder logic
-//
-//                if (index != 0) {
-//                    append(";")
-//                }
-//                append("$x,$y")
-//            }
-//        }
-//    }
-
-    companion object {
-
     }
 }
